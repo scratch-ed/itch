404afd0ab91af9e929cb77494a80f37498a4434d
Nils Mak <nils.mak@gmail.com>
2018-10-22 16:07:13 +0200

    mocha tests

--- result-judge/test/square-test.js
+++ result-judge/test/square-test.js
@@ -4,19 +4,27 @@ const runCode = require("./runCode.js");
 const expect = require('chai').expect;
 
 const maxExecutionTime = 10000;
+const fileName = 'square-segments-turned.sb3';
 
 describe('square', function() {
     this.timeout(maxExecutionTime);
     let logData;
 
     before(async function() {
-        logData = await runCode.getLogData('square.sb3');
+        logData = await runCode.getLogData(fileName);
         return logData;
     });
 
     describe('#findSquare', () => {
         it('should detect a square', async () => {
             expect(testFunctions.detectSquare(logData)).to.be.true;
+        })
+    });
+
+    describe('#checkColor', () => {
+        it('should be drawn in blue', async () => {
+            const blue = [0,0,1,1];
+            expect(testFunctions.detectColor(logData)).to.deep.equal(blue);
         })
     });
 });
