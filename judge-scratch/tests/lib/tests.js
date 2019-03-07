f2e2c473102c965342c16cd24249948c8404e6d9
Nils Mak <nils.mak@gmail.com>
2019-03-07 15:18:03 +0100

    start of dodona judge

---
+++ judge-scratch/tests/lib/tests.js
@@ -0,0 +1,19 @@
+
+
+export class Tests {
+  constructor() {
+    this.tests = [];
+    this.id = 0;
+    this.numberOfCorrectTests = 0;
+  }
+
+  add(isCorrect, msgIfCorrect, msgIfWrong) {
+    if (isCorrect) {
+      this.tests.push({id:this.id, correct: true, msg: msgIfCorrect});
+      this.numberOfCorrectTests++;
+    } else {
+      this.tests.push({id:this.id, correct: false, msg: msgIfWrong});
+    }
+    this.id++;
+  }
+}
