e3a255ca4e5a32894c99c64331ec14138ccbeaf3
Nils Mak <nils.mak@gmail.com>
2019-03-11 13:45:41 +0100

    added some extra examples as tests-during-execution

--- judge-scratch/scratch/judge/scratchSimulationEvent.js
+++ judge-scratch/scratch/judge/scratchSimulationEvent.js
@@ -1,70 +1,120 @@
 class ScratchSimulationEvent extends SimulationEvent {
 
-  clickSprite(spriteName, delay = 0) {
+    clickSprite(spriteName, delay = 0) {
 
-    return this.next(() => {
-      console.log(`click ${spriteName}`);
-      // fetch the target
-      let _sprite = Scratch.vm.runtime.getSpriteTargetByName(spriteName);
+        return this.next(() => {
+            console.log(`click ${spriteName}`);
+            // fetch the target
+            let _sprite = Scratch.vm.runtime.getSpriteTargetByName(spriteName);
 
-      // simulate mouse click by explicitly triggering click event on
-      // the target
-      if(spriteName !== 'Stage'){
-        Scratch.vm.runtime.startHats('event_whenthisspriteclicked', null, _sprite);
-      }
-    }, delay);
+            // simulate mouse click by explicitly triggering click event on
+            // the target
+            if (spriteName !== 'Stage') {
+                Scratch.vm.runtime.startHats('event_whenthisspriteclicked', null, _sprite);
+            }
+        }, delay);
 
-  }
+    }
 
-  pressKey(key, delay = 0) {
+    pressKey(key, delay = 0) {
 
-    return this.next(() => {
-      console.log(`press ${key}`);
-      let data = {key: key, isDown: true};
-      Scratch.vm.runtime.ioDevices.keyboard.postData(data);
-    }, delay);
+        return this.next(() => {
+            console.log(`press ${key}`);
+            let data = {key: key, isDown: true};
+            Scratch.vm.runtime.ioDevices.keyboard.postData(data);
+        }, delay);
 
-  }
+    }
 
-  testCostume(spriteName, correctCostumeName, delay = 50) {
-      return this.next(() => {
-          let _sprite = Scratch.vm.runtime.getSpriteTargetByName(spriteName);
-          if (_sprite) {
-              let costumeNr = _sprite.currentCostume;
-              let costumeName = _sprite.sprite.costumes_[costumeNr].name;
+    testCostume(spriteName, correctCostumeName, delay = 50) {
+        return this.next(() => {
+            let _sprite = Scratch.vm.runtime.getSpriteTargetByName(spriteName);
+            if (_sprite) {
+                let costumeNr = _sprite.currentCostume;
+                let costumeName = _sprite.sprite.costumes_[costumeNr].name;
 
-              if (costumeName === correctCostumeName) {
-                  console.log('dodona', `Correct: de sprite heeft het kostuum: ${costumeName}`);
-              } else {
-                  console.log('dodona', `Fout: de sprite heeft het kostuum: ${costumeName}, maar moest het kostuum: ${correctCostumeName} hebben.`);
-              }
-          }
-          else {
-            console.log('error: no sprite found');
-          }
+                if (costumeName === correctCostumeName) {
+                    console.log('dodona', `Correct: de sprite heeft het kostuum: ${costumeName}`);
+                } else {
+                    console.log('dodona', `Fout: de sprite heeft het kostuum: ${costumeName}, maar moest het kostuum: ${correctCostumeName} hebben.`);
+                }
+            }
+            else {
+                console.log('error: no sprite found');
+            }
 
-      }, delay);
-  }
+        }, delay);
+    }
 
-  end(delay = 200) {
+    testXCoordinate(spriteName, correctX, delay = 50) {
+        return this.next(() => {
+            let _sprite = Scratch.vm.runtime.getSpriteTargetByName(spriteName);
+            if (_sprite) {
+                let x = _sprite.x;
+                if (Math.abs(x - correctX) < 0.01) {
+                    console.log('dodona', `Correct: de sprite heeft x-coordinaat: ${correctX}`);
+                } else {
+                    console.log('dodona', `Fout: de sprite heeft x-coordinaat: ${x}, maar moest x-coordinaat: ${correctX} hebben.`);
+                }
+            }
+            else {
+                console.log('error: no sprite found');
+            }
+        }, delay);
+    }
 
-    return this.next(() => {
-      console.log("Finished simulation");
-      // stop all Scratch processes
-      Scratch.vm.stopAll();
+    testYCoordinate(spriteName, correctY, delay = 50) {
+        return this.next(() => {
+            let _sprite = Scratch.vm.runtime.getSpriteTargetByName(spriteName);
+            if (_sprite) {
+                let y = _sprite.y;
+                if (Math.abs(y - correctY) < 0.01) {
+                    console.log('dodona', `Correct: de sprite heeft x-coordinaat: ${correctY}`);
+                } else {
+                    console.log('dodona', `Fout: de sprite heeft x-coordinaat: ${y}, maar moest x-coordinaat: ${correctY} hebben.`);
+                }
+            }
+            else {
+                console.log('error: no sprite found');
+            }
+        }, delay);
+    }
 
-      Scratch.executionEnd.resolve();
-      Scratch.simulationEnd.resolve();
+    testCoordinates(spriteName, correctCoordinates, delay = 50) {
+        return this.next(() => {
+            let _sprite = Scratch.vm.runtime.getSpriteTargetByName(spriteName);
+            if (_sprite) {
+                let d1 = {x: _sprite.x, y: _sprite.y};
+                if (isEqual(d1, correctCoordinates)) {
+                    console.log('dodona', `Correct: de sprite heeft coordinaten: (${d1.x}, ${d1.y}`);
+                } else {
+                    console.log('dodona', `Fout: de sprite heeft coordinaten: (${d1.x}, ${d1.y}, maar moest coordinaten: (${correctCoordinates.x}, ${correctCoordinates.y}) hebben.`);                }
+            }
+            else {
+                console.log('error: no sprite found');
+            }
+        }, delay);
+    }
 
-    }, delay);
+    end(delay = 200) {
 
-  }
+        return this.next(() => {
+            console.log("Finished simulation");
+            // stop all Scratch processes
+            Scratch.vm.stopAll();
 
-  evaluate(action, delay = 0) {
+            Scratch.executionEnd.resolve();
+            Scratch.simulationEnd.resolve();
 
-    // this is just an alias for next (rethink passing environment and observations to action)
-    return this.next(action, delay);
+        }, delay);
 
-  }
+    }
+
+    evaluate(action, delay = 0) {
+
+        // this is just an alias for next (rethink passing environment and observations to action)
+        return this.next(action, delay);
+
+    }
 
 }
