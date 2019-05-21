3c0454169371e80a1e3b0bc416b0d146b62c62ec
Nils Mak <nils.mak@gmail.com>
2019-05-21 17:21:39 +0200

    voorbeeld testplan heksenjacht voor in introductie

--- tests/vpw2017/07_heksenjacht_eenvoudig_test.js
+++ tests/vpw2017/07_heksenjacht_eenvoudig_test.js
@@ -1,5 +1,9 @@
-function duringExecution() {
+function beforeExecution(templateJSON, testJSON) {
+    // Controleer of het ingediende project van de leerling een sprite heeft met als naam 'Heks'
+    addCase('De sprite Heks is niet hernoemd', containsSprite(testJSON, 'Heks'), 'De sprite met als naam Heks werd niet teruggevonden in het project')
+}
 
+function duringExecution() {
     actionTimeout = 7000; //Indien een actie langer dan 7 seconden duurt, geef een timeout error.
 
     let heksPositie = {}; //We slaan de positie van de heks op om op te testen tijdens de uitvoering
@@ -34,27 +38,16 @@ function duringExecution() {
             heksPositie['y'] = heks.y;
             return heeftBewogen;
         })
-        .wait(1000) // wacht een seconde voor de volgende positie
-        .test('De heks is veranderd van positie', 'De heks is niet van positie veranderd twee seconden na de klik', (log) => {
-            let heks = log.sprites.getSprite('Heks');
-            let heeftBewogen = heks.x !== heksPositie['x'] || heks.y !== heksPositie['y'];
-            heksPositie['x'] = heks.x;
-            heksPositie['y'] = heks.y;
-            return heeftBewogen;
-        })
         .end();
 
     scratch.start();
 }
 
 function afterExecution() {
-
     // Gebruik best een lus om elke seconde de heks te verplaatsen
     addCase('Gebruik van een lus', log.blocks.containsBlock('control_repeat'), 'Er werd geen herhalingslus gebruikt');
 
     // De code in de lus wordt minstens 2 keer herhaald
     addCase('Correcte gebruik van de lus', log.blocks.numberOfExecutions('control_repeat') > 2, 'De code in de lus werd minder dan 2 keer herhaald');
-
-
 }
 
