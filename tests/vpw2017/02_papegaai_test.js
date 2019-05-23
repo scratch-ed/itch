892ea421ac1aaeee0dac188c5b39630e557ada4a
Nils Mak <nils.mak@gmail.com>
2019-05-23 08:20:35 +0200

    Update 02_papegaai_test.js

--- tests/vpw2017/02_papegaai_test.js
+++ tests/vpw2017/02_papegaai_test.js
@@ -50,7 +50,7 @@ function afterExecution() {
                 let raaktRand = (papegaai.x + papegaai.bounds.width / 2 > 230) || (papegaai.x - papegaai.bounds.width / 2 < -230);
                 addCase('De papegaai raakt de rand bij het veranderen van richting', raaktRand, 'De papegaai is veranderd van richting zonder de rand te raken van het speelveld');
                 // Test of de papegaai altijd van links naar rechts en omgekeerd beweegt
-                let vliegtHorizontaal = (papegaai.direction === 90 || papegaai.direction === -90);
+                let vliegtHorizontaal = (sprite.direction === 90 || sprite.direction === -90);
                 addCase('De papegaai vliegt horizontaal', vliegtHorizontaal, 'De richting van de papegaai is niet 90 of -90, de papegaai vliegt niet horizontaal.');
             }
         }
