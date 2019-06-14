074759cafddbc1828ff44c561456962feaedcfcf
nnmak <nils.mak@gmail.com>
2019-06-14 12:19:14 +0200

    Update 02_papegaai_test.js

--- tests/vpw2017/02_papegaai_test.js
+++ tests/vpw2017/02_papegaai_test.js
@@ -43,23 +43,28 @@ function afterExecution() {
             // Test of de papegaai de rand raakt
             let papegaai = previousFrame.getSprite('Papegaai');
             let raaktRand = (papegaai.x + papegaai.bounds.width / 2 > 230) || (papegaai.x - papegaai.bounds.width / 2 < -230);
-            addCase('De papegaai raakt de rand bij het veranderen van richting', raaktRand, 'De papegaai is veranderd van richting zonder de rand te raken van het speelveld');
+            addCase('De papegaai raakt de rand bij het veranderen van richting', raaktRand, 
+                    'De papegaai is veranderd van richting zonder de rand te raken van het speelveld');
             // Test of de papegaai altijd van links naar rechts en omgekeerd beweegt
             let vliegtHorizontaal = (sprite.direction === 90 || sprite.direction === -90);
-            addCase('De papegaai vliegt horizontaal', vliegtHorizontaal, 'De richting van de papegaai is niet 90 of -90, de papegaai vliegt niet horizontaal.');
+            addCase('De papegaai vliegt horizontaal', vliegtHorizontaal, 
+                    'De richting van de papegaai is niet 90 of -90, de papegaai vliegt niet horizontaal.');
         }
         previousFrame = frame;
     }
 
-    addCase('De papegaai veranderde van richting', directions.length > 2, `De papegaai moet minstens twee veranderen van richting, maar is maar ${directions.length} keer veranderd`);
+    addCase('De papegaai veranderde van richting', directions.length > 2, 
+            `De papegaai moet minstens twee veranderen van richting, maar is maar ${directions.length} keer veranderd`);
 
     // De papegaai verandert van kostuum tijdens het vliegen
     let costumeChanges = log.getCostumeChanges('Papegaai');
-    addCase('Papegaai klappert met vleugels', costumeChanges.length > 30, `De Papegaai moet constant wisselen tussen de kostuums 'VleugelsOmhoog' en 'VleugelsOmlaag'`);
+    addCase('Papegaai klappert met vleugels', costumeChanges.length > 30, 
+            `De Papegaai moet constant wisselen tussen de kostuums 'VleugelsOmhoog' en 'VleugelsOmlaag'`);
 
     // Gebruik best een lus de papegaai te bewegen en van kostuum te veranderen.
     addCase('Gebruik van een lus', log.blocks.containsLoop(), 'Er werd geen herhalingslus gebruikt');
 
     // De code in de lus wordt minstens 2 keer herhaald
-    addCase('Correcte gebruik van de lus', log.blocks.numberOfExecutions('control_forever') > 2, 'De code in de lus werd minder dan 2 keer herhaald');
+    addCase('Correcte gebruik van de lus', log.blocks.numberOfExecutions('control_forever') > 2, 
+            'De code in de lus werd minder dan 2 keer herhaald');
 }
