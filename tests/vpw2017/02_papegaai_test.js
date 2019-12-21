6c496a13923c7b35af65392c84eb503e9029f6ce
Christophe <xtofs@Christophes-MacBook-Pro.local>
2019-12-21 23:15:56 +0100

    door de ; op het einde van de lijn werkt de test niet

--- tests/vpw2017/02_papegaai_test.js
+++ tests/vpw2017/02_papegaai_test.js
@@ -11,7 +11,7 @@ function duringExecution() {
     scratch.eventScheduling
         .wait(1000)
         .test('De papegaai heeft nog niet bewogen', 'De papegaai mag niet bewegen voor er op geklikt wordt', 
-              log => !log.hasSpriteMoved('Papegaai');
+              (log) => { !log.hasSpriteMoved('Papegaai') }
         )
         .clickSprite({spriteName: 'Papegaai', sync: false}) // De eerste klik laat de papegaai starten met bewegen.
         .wait(3000)
