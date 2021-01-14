c2fa7d904c1d73d09843f641f3d2c46ee56aae05
Niko Strijbol <strijbol.niko@gmail.com>
2021-01-14 14:44:26 +0100

    Fix square test

--- tests/plans/vpw2017/03_teken_een_vierkant_test.js
+++ tests/plans/vpw2017/03_teken_een_vierkant_test.js
@@ -19,7 +19,8 @@ function afterExecution() {
     }
 
     // Elke zijde van het vierkant heeft lengte 200
-    addCase('Zijdes hebben lengte 200', 200 === vierkanten[0].length, 'De zijdes van het vierkant hebben niet lengte 200');
+    const correct = equals(200, vierkanten[0].length);
+    addCase('Zijdes hebben lengte 200', correct, 'De zijdes van het vierkant hebben niet lengte 200');
 
     // Er werd gebruik gemaakt van de pen
     addCase('De pen werd gebruikt', log.blocks.containsBlock('pen_penDown'), 'Het blok pen_down werd niet gebruikt in de code');
