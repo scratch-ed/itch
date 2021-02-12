4d829becc55d1d3c9b6de51b25111cd7f3acfd16
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2021-02-12 11:22:33 +0100

    pass stream to judge and api option

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,43 @@
-// TODO: validation
+const { PassThrough } = require('stream');
+const { Judge } = require('../../judge');
+
+const COMMANDS = Object.fromEntries(
+  Object.keys(require('../../utils/dodona-schema').definitions).map((key) => [
+    [key.toUpperCase().replace('-', '_')],
+    key,
+  ]),
+);
+
+const ALLOWED_COMMANDS = [
+  COMMANDS.START_TAB,
+  COMMANDS.START_TESTCASE,
+  COMMANDS.CLOSE_TEST,
+  COMMANDS.CLOSE_TESTCASE,
+  COMMANDS.CLOSE_TAB,
+];
+
 function setupRoutes(server) {
-  server.post('/test', (req, res, next) => {
-    //const { testPlan, template, submitted } = req.body;
+  server.post('/test', async (req, res, next) => {
+    const { testplan } = req.body;
+    const { templateFile, testFile } = req.files;
 
-    // TODO: judge.runTests here
+    res.setHeader('Content-Type', 'text/plain');
 
-    res.send({
-      result:
-        'Je bent nog niet begonnen met programmeren! Neem een blokje en go go go!',
+    const pass = new PassThrough();
+    pass.pipe(res);
+
+    const judge = new Judge(testplan, { fromApi: true }, (judgeObject) => {
+      if (!ALLOWED_COMMANDS.includes(judgeObject.command)) {
+        return;
+      }
+
+      pass.write(JSON.stringify(judgeObject));
+      pass.resume();
     });
+
+    await judge.run(templateFile.path, testFile.path);
+
+    pass.end();
     next();
   });
 }
