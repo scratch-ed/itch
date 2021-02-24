1f05151a6c94b53b50f11a94442fe721e12c7b06
Niko Strijbol <strijbol.niko@gmail.com>
2021-02-24 17:36:50 +0100

    Tests for ton

--- demo/zombiebal.js
+++ demo/zombiebal.js
@@ -6,7 +6,7 @@
  */
 
 function beforeExecution(template, submission, output) {
-  output.startTestTab("Basiscontroles");
+  output.startTestTab('Basiscontroles');
   output.addTest('Niet geprutst met andere sprites',
     false,
     template.hasAddedSprites(submission) || template.hasRemovedSprites(submission),
@@ -16,7 +16,7 @@ function beforeExecution(template, submission, output) {
 }
 
 /**
- * @param {Object} data 
+ * @param {Object} data
  * @param {Evaluation} e
  * @param {ScheduledEvent} events
  */
@@ -27,7 +27,7 @@ function testSprite(events, data, e) {
   // First do the checks on the Roy sprite.
   return events
     .log(() => {
-      e.output.startTestTab(`Testen voor ${name}`)
+      e.output.startTestTab(`Testen voor ${name}`);
       const sprite = e.log.current.getSprite(name);
       originalPosition = {
         x: sprite.x,
@@ -40,8 +40,6 @@ function testSprite(events, data, e) {
       const sprite = e.log.current.getSprite(name);
       // We can't know how many steps will be set.
       e.output.startTestCase(`${name} beweegt omlaag`);
-      console.log("Found", sprite);
-      console.log("Expected", originalPosition);
       e.output.addOneTest(
         true,
         sprite.y < originalPosition.y,
@@ -69,11 +67,6 @@ function testSprite(events, data, e) {
         `Als op ${up} gedrukt wordt, moet ${name} 10 stappen omhoog zetten.`
       );
       e.output.addOneTest(
-        originalPosition.y,
-        sprite.y,
-        `${name} moet even snel omhoog als naar beneden gaan.`
-      );
-      e.output.addOneTest(
         originalPosition.x,
         sprite.x,
         `${name} mag niet horizontaal bewegen.`
@@ -83,20 +76,120 @@ function testSprite(events, data, e) {
     });
 }
 
+let loser;
+let winner;
+
+/**
+ * @param {Evaluation} e
+ * @param {ScheduledEvent} events
+ */
+function testTon(events, e) {
+  let touchedSprite;
+  let secondSprite;
+  let limit;
+  let limitSecond;
+  
+  // Wait until the ton is at the right position, and then
+  // move the sprite to that position.
+  events.wait(sprite('Ton').toReach((x, _y) => x > 60 || x < -60))
+    .log(() => {
+      const ton = e.vm.runtime.getSpriteTargetByName('Ton');
+      let sprite;
+      if (ton.x < 0) {
+        sprite = e.vm.runtime.getSpriteTargetByName('Roy');
+        touchedSprite = 'Roy';
+        secondSprite = 'Rob';
+        limit = (x, _y) => x > 60;
+        limitSecond = (x, _y) => x < -60;
+      } else {
+        sprite = e.vm.runtime.getSpriteTargetByName('Rob');
+        touchedSprite = 'Rob';
+        secondSprite = 'Roy';
+        limit = (x, _y) => x < -60;
+        limitSecond = (x, _y) => x > 60;
+      }
+      if (!sprite) {
+        throw new Error(`Could not find target Ton`);
+      }
+      sprite.setXY(sprite.x, ton.y);
+    })
+    // TODO: this should also be a test somehow.
+    .wait(sprite('Ton').toTouch(() => touchedSprite))
+    .log(() => {
+      e.output.startTestTab("Testen voor ton")
+      e.output.addTest(`Ton raakt ${touchedSprite}`, true, true);
+    })
+    .wait(sprite('Ton').toReach((x, y) => limit(x, y)))
+    .log(() => {
+      const ton = e.vm.runtime.getSpriteTargetByName('Ton');
+      const sprite = e.vm.runtime.getSpriteTargetByName(secondSprite);
+      sprite.setXY(sprite.x, ton.y);
+    })
+    .wait(sprite('Ton').toTouch(() => secondSprite))
+    .log(() => {
+      e.output.addTest(`Ton raakt ${secondSprite}`, true, true);
+    })
+    .forEach(_.range(4), (ev) => {
+      // Wait until we get to the next one.
+      return ev.wait(sprite('Ton').toReach((x, y) => limitSecond(x, y)))
+        .log(() => {
+          const ton = e.vm.runtime.getSpriteTargetByName('Ton');
+          const sprite = e.vm.runtime.getSpriteTargetByName(touchedSprite);
+          sprite.setXY(sprite.x, ton.y);
+        })
+        .wait(sprite('Ton').toTouch(() => touchedSprite))
+        .log(() => {
+          e.output.addTest(`Ton raakt ${touchedSprite}`, true, true);
+          [secondSprite, touchedSprite] = [touchedSprite, secondSprite];
+          [limitSecond, limit] = [limit, limitSecond];
+        })
+    })
+    .wait(sprite('Ton').toReach((x, y) => limitSecond(x, y)))
+    .log(() => {
+      // Move the sprite away from the ton to make the game stop.
+      const ton = e.vm.runtime.getSpriteTargetByName('Ton');
+      const sprite = e.vm.runtime.getSpriteTargetByName(touchedSprite);
+      let newY;
+      if (ton.y > 110) {
+        newY = ton.y - 100;
+      } else {
+        newY = ton.y + 100;
+      }
+      sprite.setXY(sprite.x, newY);
+      loser = touchedSprite;
+      winner = secondSprite;
+      e.output.closeTestTab();
+    })
+    .wait(sprite('Ton').toTouch(() => `${loser}'s Doel`))
+    .wait(2000)
+    .end();
+}
+
 /** @param {Evaluation} e */
 function duringExecution(e) {
-  e.actionTimeout = 30000;
+  e.actionTimeout = 300000;
   // e.acceleration = 10;
   // e.eventAcceleration = 1;
-  
-  let events = e.scheduler
+
+  const originalEvents = e.scheduler
     .greenFlag(false)
     .wait(5000); // Wait on start of the game.
-  
+
   // We cannot do these concurrently, since the key presses will be mixed then.
-  events = testSprite(events,{ name: 'Roy', up: 'q', down: 'w' }, e);
-  events = testSprite(events, { name: 'Rob', up: 'j', down: 'n' }, e);
-  
-  
-  events.end();
+  const events = testSprite(originalEvents, { name: 'Roy', up: 'q', down: 'w' }, e);
+  testSprite(events, { name: 'Rob', up: 'j', down: 'n' }, e);
+
+  testTon(originalEvents, e);
+}
+
+/** @param {Evaluation} e */
+function afterExecution(e) {
+  const sayEvents = e.log.events.filter({ type: 'say' });
+  const data = _.last(sayEvents).data;
+  e.output.addTest(
+    `De ton zegt ${winner} scoort!`,
+    `${winner} scoort!`,
+    data.text,
+    "De ton kondigt de score aan."
+  )
 }
