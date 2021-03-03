abe57dc26eccb8aa955b4915323bd824311835f4
Niko Strijbol <strijbol.niko@gmail.com>
2021-03-03 19:38:30 +0100

    Use new testplan API

--- demo/zombiebal.js
+++ demo/zombiebal.js
@@ -2,46 +2,38 @@
  * Check that the original sprites were not changed.
  * @param {Project} template - The template project.
  * @param {Project} submission - The submission project.
- * @param {ResultManager} output - The output manager.
+ * @param {Evaluation} e - The output manager.
  */
 
-function beforeExecution(template, submission, output) {
-  output.startTestTab('Basiscontroles');
-  output.addTest('Niet geprutst met andere sprites',
-    false,
-    template.hasAddedSprites(submission) || template.hasRemovedSprites(submission),
-    'Er is iets veranderd aan de ingebouwde sprites, waar je niets moet aan veranderen.'
-  );
-  output.closeTestTab();
-  
-  output.startTestTab("Controle op blokjes");
-  output.addTest(
-    'Oneindige lus gebruikt bij Rob',
-    true,
-    submission.sprite('Rob').hasBlock('control_forever')
-  );
-  output.addTest(
-    'Oneindige lus gebruikt bij Roy',
-    true,
-    submission.sprite('Roy').hasBlock('control_forever')
-  );
-  
-  // Check the ton.
-  const ton = submission.sprite('Ton');
-  const callIndex = ton.blocks.findIndex(block => { 
-    return block.opcode === 'procedures_call' && block.calledProcedureName === 'Richt naar Roy of Rob'; 
+function beforeExecution(template, submission, e) {
+  e.describe('Basiscontroles', l => {
+    l.test('Niet geprutst met andere sprites', l => {
+      l.expect(template.hasAddedSprites(submission)).toBe(false);
+      l.expect(template.hasRemovedSprites(submission)).toBe(false);
+    })
   });
-  const loopIndex = ton.blocks.findIndex(block =>{
-    return block.opcode === 'control_repeat_until'
-  });
-  output.addTest(
-    'Ton richt naar spelers',
-    true,
-    callIndex < loopIndex,
-    'De ton moet eerst naar Roy of Rob gericht worden.'
-  );
   
-  output.closeTestTab();
+  e.describe("Statische controles op blokjes", l => {
+    l.test('Oneindige lus gebruikt bij Rob', l => {
+      l.expect(submission.sprite('Rob').hasBlock('control_forever')).toBe(true);
+    });
+    l.test('Oneindige lus gebruikt bij Roy', l => {
+      l.expect(submission.sprite('Roy').hasBlock('control_forever')).toBe(true);
+    });
+
+    // Check the ton.
+    const ton = submission.sprite('Ton');
+    const callIndex = ton.blocks.findIndex(block => {
+      return block.opcode === 'procedures_call' && block.calledProcedureName === 'Richt naar Roy of Rob';
+    });
+    const loopIndex = ton.blocks.findIndex(block =>{
+      return block.opcode === 'control_repeat_until'
+    });
+    
+    l.test('Ton richt naar spelers', l => {
+      l.expect(callIndex < loopIndex).toBe(true);
+    });
+  });
 }
 
 /**
@@ -56,7 +48,7 @@ function testSprite(events, data, e) {
   // First do the checks on the Roy sprite.
   return events
     .log(() => {
-      e.output.startTestTab(`Testen voor ${name}`);
+      e.output.startTestContext(`Testen voor ${name}`);
       const sprite = e.log.current.getSprite(name);
       originalPosition = {
         x: sprite.x,
@@ -68,23 +60,17 @@ function testSprite(events, data, e) {
     .log(() => {
       const sprite = e.log.current.getSprite(name);
       // We can't know how many steps will be set.
-      e.output.startTestCase(`${name} beweegt omlaag`);
-      e.output.addOneTest(
-        180,
-        sprite.direction,
-        `${name} moet omlaag gericht zijn.`
-      )
-      e.output.addOneTest(
-        true,
-        sprite.y < originalPosition.y,
-        `Als op ${down} gedrukt wordt, moet ${name} 10 stappen omlaag zetten.`
-      );
-      e.output.addOneTest(
-        originalPosition.x,
-        sprite.x,
-        `${name} mag niet horizontaal bewegen.`
-      );
-      e.output.closeTestCase();
+      e.test(`${name} moet omlaag gericht zijn`, l => {
+        l.expect(sprite.direction).toBe(180);
+      });
+      e.test(`${name} beweegt omlaag`, l => {
+        l.expect(sprite.y < originalPosition.y)
+          .withError(`Als op ${down} gedrukt wordt, moet ${name} 10 stappen omlaag zetten.`)
+          .toBe(true);
+        l.expect(sprite.x)
+          .withError(`${name} mag niet horizontaal bewegen.`)
+          .toBe(originalPosition.x);
+      });
       latestPosition = {
         x: sprite.x,
         y: sprite.y
@@ -94,24 +80,18 @@ function testSprite(events, data, e) {
     .log(() => {
       const sprite = e.log.current.getSprite(name);
       // We can't know how many steps will be set.
-      e.output.startTestCase(`${name} beweegt omhoog`);
-      e.output.addOneTest(
-        true,
-        sprite.y > latestPosition.y,
-        `Als op ${up} gedrukt wordt, moet ${name} 10 stappen omhoog zetten.`
-      );
-      e.output.addOneTest(
-        0,
-        sprite.direction,
-        `${name} moet omhoog gericht zijn.`
-      )
-      e.output.addOneTest(
-        originalPosition.x,
-        sprite.x,
-        `${name} mag niet horizontaal bewegen.`
-      );
-      e.output.closeTestCase();
-      e.output.closeTestTab();
+      e.test(`${name} beweegt omhoog`, l => {
+        l.expect(sprite.y > latestPosition.y)
+          .withError(`Als op ${up} gedrukt wordt, moet ${name} 10 stappen omhoog zetten.`)
+          .toBe(true);
+        l.expect(sprite.direction)
+          .withError(`${name} moet omhoog gericht zijn.`)
+          .toBe(0);
+        l.expect(sprite.x)
+          .withError(`${name} mag niet horizontaal bewegen.`)
+          .toBe(originalPosition.x);
+      });
+      e.output.closeTestContext();
     });
 }
 
@@ -155,8 +135,10 @@ function testTon(events, e) {
     // TODO: this should also be a test somehow.
     .wait(sprite('Ton').toTouch(() => touchedSprite))
     .log(() => {
-      e.output.startTestTab("Testen voor ton")
-      e.output.addTest(`Ton raakt ${touchedSprite}`, true, true);
+      e.output.startTestContext("Testen voor ton");
+      e.test(`Ton raakt ${touchedSprite}`, l => {
+        l.accept();
+      });
     })
     .wait(sprite('Ton').toReach((x, y) => limit(x, y)))
     .log(() => {
@@ -166,7 +148,9 @@ function testTon(events, e) {
     })
     .wait(sprite('Ton').toTouch(() => secondSprite))
     .log(() => {
-      e.output.addTest(`Ton raakt ${secondSprite}`, true, true);
+      e.test(`Ton raakt ${secondSprite}`, l => {
+        l.accept();
+      });
     })
     .forEach(_.range(4), (ev) => {
       // Wait until we get to the next one.
@@ -178,7 +162,9 @@ function testTon(events, e) {
         })
         .wait(sprite('Ton').toTouch(() => touchedSprite))
         .log(() => {
-          e.output.addTest(`Ton raakt ${touchedSprite}`, true, true);
+          e.test(`Ton raakt ${touchedSprite}`, l => {
+            l.accept();
+          });
           [secondSprite, touchedSprite] = [touchedSprite, secondSprite];
           [limitSecond, limit] = [limit, limitSecond];
         })
@@ -197,7 +183,7 @@ function testTon(events, e) {
       sprite.setXY(sprite.x, newY);
       loser = touchedSprite;
       winner = secondSprite;
-      e.output.closeTestTab();
+      e.output.closeTestContext();
     })
     .wait(sprite('Ton').toTouch(() => `${loser}'s Doel`))
     .wait(2000)
@@ -218,6 +204,7 @@ function duringExecution(e) {
   const events = testSprite(originalEvents, { name: 'Roy', up: 'q', down: 'w' }, e);
   testSprite(events, { name: 'Rob', up: 'j', down: 'n' }, e);
 
+  
   testTon(originalEvents, e);
 }
 
@@ -225,10 +212,7 @@ function duringExecution(e) {
 function afterExecution(e) {
   const sayEvents = e.log.events.filter({ type: 'say' });
   const data = _.last(sayEvents).data;
-  e.output.addTest(
-    `De ton zegt ${winner} scoort!`,
-    `${winner} scoort!`,
-    data.text,
-    "De ton kondigt de score aan."
-  )
+  e.test(`De ton zegt ${winner} scoort!`, l => {
+    l.expect(data.text).toBe(`${winner} scoort!`);
+  });
 }
