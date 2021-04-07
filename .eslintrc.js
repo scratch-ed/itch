87500ab9ae77104742606f66753db9f3d837faf3
Niko Strijbol <strijbol.niko@gmail.com>
2021-04-07 14:58:25 +0200

    Fix formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
     'jest/globals': true,
   },
-  extends: ["standard", 'prettier'],
+  extends: ['standard', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
