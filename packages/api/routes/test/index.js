91a6862805431ac8a12a97ddaebc31b42088f2d0
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2021-05-28 16:01:24 +0200

    add non streamable response

--- packages/api/routes/test/index.js
+++ packages/api/routes/test/index.js
@@ -1,6 +1,9 @@
+const { runJudge } = require('itch-runner');
 const testStream = require('./stream');
 
-const test = async (req, res, next, browser) => {
+const parseCommands = require('../../utils/parseCommands');
+
+const test = async (req, reply, next, browser) => {
   const { testplan } = req.body;
   const { templateFile, testFile } = req.files;
   const { accept } = req.headers;
@@ -9,10 +12,40 @@ const test = async (req, res, next, browser) => {
 
   // stream mode
   if (accept === 'application/octet-stream') {
-    await testStream(res, { testplan, templateFile, testFile, page });
+    await testStream(reply, { testplan, templateFile, testFile, page });
     await page.close();
     next();
   }
+
+  const output = [];
+
+  try {
+    await runJudge({
+      testplan: { content: testplan },
+      template: templateFile.path,
+      solution: testFile.path,
+      page,
+      out: (judgeObject) =>
+        parseCommands(judgeObject, {
+          onChunkFinished: (chunk) => {
+            output.push(chunk);
+          },
+          onError: () => {
+            throw new Error(
+              'the judge internally failed, check the logs for more information',
+            );
+          },
+        }),
+    });
+  } catch (err) {
+    console.log(err);
+
+    reply.code(500).send(err);
+  }
+
+  await page.close();
+  reply.send(output);
+  next();
 };
 
 module.exports = test;
