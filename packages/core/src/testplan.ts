1b2910230f33f8d9bb933b0631841856cf1c35d8
Niko Strijbol <strijbol.niko@gmail.com>
2021-06-11 13:18:59 +0200

    Fix test

--- packages/core/src/testplan.ts
+++ packages/core/src/testplan.ts
@@ -283,6 +283,17 @@ function fixHatBlock(filteredBlocks: Sb3Block[], hatBlock: Sb3Block) {
   filteredBlocks[solutionIndex] = hatBlock;
 }
 
+// function difference(object, base) {
+//   function changes(object, base) {
+//     return _.transform(object, function(result, value, key) {
+//       if (!_.isEqual(value, base[key])) {
+//         result[key] = (_.isObject(value) && _.isObject(base[key])) ? changes(value, base[key]) : value;
+//       }
+//     });
+//   }
+//   return changes(object, base);
+// }
+
 export class OneHatAllowedTest {
   private template: Project;
   private submission: Project;
