12c728071d17ef264cae237578df48409da127ce
Niko Strijbol <strijbol.niko@gmail.com>
2022-02-03 15:44:40 +0100

    Clean up dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
     'jest/globals': true,
   },
-  extends: ['standard', 'prettier'],
+  extends: ['prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
