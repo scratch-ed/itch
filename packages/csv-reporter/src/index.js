33080dfbd4b103d0cd180160b29c184bacef6707
Niko Strijbol <strijbol.niko@gmail.com>
2023-02-01 13:31:33 +0100

    Write markdown summary

--- packages/csv-reporter/src/index.js
+++ packages/csv-reporter/src/index.js
@@ -8,7 +8,7 @@ class CsvReporter {
   }
 
   onRunComplete(contexts, results) {
-    const data = [['Oefening', 'Level', 'Test', 'Resultaat']];
+    let data = [];
     const regex = /Level ([0-9]+)/i;
 
     for (const result of results.testResults) {
@@ -32,7 +32,38 @@ class CsvReporter {
       }
     }
 
-    let stringData = '';
+    const correct = data.filter((d) => d[3] === 'passed').length;
+    const wrong = data.filter((d) => d[3] === 'failed').length;
+    // Create results as markdown file.
+    // This can then be posted to the GitHub job summary.
+    let markdown = `
+### Samenvatting
+|         | Aantal | %     |
+|---------|--------|-------|
+| Totaal  | ${data.length} | 100% |
+| Werkend | ${correct} | ${((correct / data.length) * 100).toFixed(2)} |
+| Kapot   | ${wrong} | ${((wrong / data.length) * 100).toFixed(2)} |
+
+### Details
+| Oefening | Level | Test  | Resultaat |
+|---------|--------|-------|-----------|
+    `;
+
+    for (const line of data) {
+      let status;
+      if (line[3] === 'failed') {
+        status = '❌ failed';
+      } else if (line[3] === 'passed') {
+        status = '✅ passed';
+      } else {
+        status = '？ pending';
+      }
+      markdown += `| ${line[0]} | ${line[1]} | ${line[2]} | ${status} |\n`;
+    }
+
+    fs.writeFileSync('results.md', markdown);
+
+    let stringData = 'Oefening, Level, Test, Resultaat';
     for (const line of data) {
       stringData += line.join(',');
       stringData += '\r\n';
